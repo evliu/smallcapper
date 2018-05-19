4f3c755ebdd8861efdee1decd7fd9151d0724056
Everest Liu <azn_logik@yahoo.com>
2018-05-18 21:27:34 -0700

    Initial code commit

---
+++ index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+const [, , words] = process.argv;
+const match = { a: 'ᴀ', b: 'ʙ', c: 'ᴄ', d: 'ᴅ', e: 'ᴇ', f: 'ғ', g: 'ɢ', h: 'ʜ', i: 'ɪ', j: 'ᴊ', k: 'ᴋ', l: 'ʟ', m: 'ᴍ', n: 'ɴ', o: 'ᴏ', p: 'ᴘ', q: 'ǫ', r: 'ʀ', s: 's', t: 'ᴛ', u: 'ᴜ', v: 'ᴠ', w: 'ᴡ', x: 'x', y: 'ʏ', z: 'ᴢ' };
+const smallcapped = words => words.split('').map((l, i) => (match[l] !== undefined) ? match[l] : l).join('');
+console.log(smallcapped(words));
